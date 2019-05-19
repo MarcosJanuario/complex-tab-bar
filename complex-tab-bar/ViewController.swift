27d1a8fda986c5621ca57516343aa4b7170f1a08
Marcos Januário <januario@prime-research.com>
2019-05-19 19:00:17 +0200

    Initial Commit

---
+++ complex-tab-bar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  complex-tab-bar
+//
+//  Created by Marcos Januário on 19.05.19.
+//  Copyright © 2019 Marcos Januário. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
